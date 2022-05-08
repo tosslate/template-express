cb6a780910d46971018784c4078c2579b0bd40c2
tossode <tossdev@189.cn>
2022-05-08 15:46:16 +0800

    tossode <82136882+tossode@users.noreply.github.com>

--- config/database.js
+++ config/database.js
@@ -1,1 +1,27 @@
-database
+const { environment } = require('./environment')
+const databaseUrl = process.env.DATABASE_URL
+
+function knexConfig() {
+  if (databaseUrl) {
+    return {
+      client: 'pg',
+      connection: databaseUrl
+    }
+  }
+
+  return {
+    client: 'sqlite3',
+    connection: {
+      filename: `./db/${environment}.sqlite`
+    },
+    useNullAsDefault: true
+  }
+}
+
+const config = knexConfig()
+const database = require('knex')(config)
+
+module.exports = {
+  database,
+  config
+}
--- config/environment.js
+++ config/environment.js
@@ -1,1 +1,5 @@
-environment
+const environment = process.env.NODE_ENV || 'development'
+
+module.exports = {
+  environment
+}

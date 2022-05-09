0b4c63b2ae6f0851faca93325be87560b9d5d7ac
tossode <tossdev@189.cn>
2022-05-09 21:44:25 +0800

    tossode <82136882+tossode@users.noreply.github.com>

--- app/controllers/posts_controller.js
+++ app/controllers/posts_controller.js
@@ -1,8 +1,9 @@
+const postProps = require('../helpers/props/post')
 const { Post } = require('../models/post')
 
 async function index(request, response) {
   const result = await Post.query().limit(12)
-  return response.json(result)
+  return response.json(postProps.eachTransform(result))
 }
 
 async function show(request, response) {
@@ -10,7 +11,7 @@ async function show(request, response) {
   const result = await Post.query().findById(postId)
 
   return result
-    ? response.json(result)
+    ? response.json(postProps.transform(result))
     : response.sendStatus(404)
 }
 

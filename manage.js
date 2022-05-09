70b5f25aa35c828c8623f4c151bede1cd18fc711
tossode <tossdev@189.cn>
2022-05-09 22:03:16 +0800

    tossode <82136882+tossode@users.noreply.github.com>

--- manage.js
+++ manage.js
@@ -1,6 +1,7 @@
+const app = require('./config/application')
 const PORT = process.env.PORT || 4567
 
-require('./config/application').listen(PORT, () => {
+app.listen(PORT, () => {
   console.log(`* Listening on http://localhost:${PORT}`)
   console.log('* Use Ctrl-C to stop')
 })

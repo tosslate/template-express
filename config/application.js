f10a2d10b86f2366c45bb15df1e07e0619ad435b
tossode <tossdev@189.cn>
2022-05-09 21:55:39 +0800

    tossode <82136882+tossode@users.noreply.github.com>

--- config/application.js
+++ config/application.js
@@ -1,16 +1,24 @@
+const { errorHandler } = require('../lib/errorhandler')
 const { graphqlHTTP } = require('express-graphql')
 const bodyParser = require('body-parser')
 const express = require('express')
 const morgan = require('morgan')
-const helmet = require('helmet')
 const schema = require('../app/graphql/schema')
-const { errorHandler } = require('../lib/errorhandler')
-// const database = require('./database')
+const resolver = require('../app/graphql/resolver')
 const { routes } = require('./routes')
 const app = express()
+
 app.use(bodyParser.json())
 app.use(morgan('dev'))
-// app.use(helmet())
 app.use(routes())
 app.use(errorHandler())
+app.use(
+  '/graphql',
+  graphqlHTTP({
+    rootValue: resolver.values(),
+    graphiql: true,
+    schema
+  })
+)
+
 module.exports = app

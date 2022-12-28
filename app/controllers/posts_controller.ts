f806c767157ce3f1e5f1e647f6a1cea8037c4950
tossode <tossdev@189.cn>
2022-12-28 22:33:28 +0800

    tosslate created a commit :sparkles:

--- app/controllers/posts_controller.ts
+++ app/controllers/posts_controller.ts
@@ -1,10 +1,10 @@
 import type { Request, Response } from 'express'
-import * as postProps from '../helpers/props/post'
 import { Post } from '../models/post'
+import { transformKeys } from '../serializers/post_serializer'
 
 export async function index(request: Request, response: Response) {
   const result = await Post.query().withGraphFetched('user').limit(12)
-  return response.json(postProps.eachTransform(result))
+  return response.json(result.map(transformKeys))
 }
 
 export async function show(request: Request, response: Response) {
@@ -12,6 +12,6 @@ export async function show(request: Request, response: Response) {
   const result = await Post.query().withGraphFetched('user').findById(postId)
 
   return result
-    ? response.json(postProps.transform(result))
+    ? response.json(transformKeys(result))
     : response.sendStatus(404)
 }
---
+++ app/serializers/user_serializer.ts
@@ -0,0 +1,5 @@
+export type UserObject = {
+  id: number
+  name: string
+  email: string
+}

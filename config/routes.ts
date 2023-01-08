1a23259768fc3ab7ced5e1bc3e498753d3703030
tossode <tossdev@189.cn>
2023-01-08 21:15:24 +0800

    tosslate created a commit :sparkles:

--- config/routes.ts
+++ config/routes.ts
@@ -1,13 +1,10 @@
 import * as categories from '../app/controllers/categories_controller'
 import * as posts from '../app/controllers/posts_controller'
-import * as users from '../app/controllers/users_controller'
 import { createRouter } from 'ramaze'
 
 export function routes() {
   return createRouter()
     .get('/posts', posts.index)
     .get('/posts/:id', posts.show)
-    .get('/users', users.index)
-    .get('/users/:id', users.show)
     .get('/categories', categories.index).routes
 }
